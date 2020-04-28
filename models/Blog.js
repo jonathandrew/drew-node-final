d61f314039763e82767fbcb9b2e67a9d36b5cc2f
jdrichardstech <jdrichardstech@gmail.com>
2020-04-28 14:24:11 -0400

    initial commit

---
+++ models/Blog.js
@@ -0,0 +1,8 @@
+const BlogSchema = new mongoose.Schema({
+  title: { type: String, default: '' },
+  author: { type: String, default: '', unique: true },
+  subject: { type: String, default: '' },
+  article: { type: String, default: '' },
+});
+
+module.exports = mongoose.model('Blog', BlogSchema);
